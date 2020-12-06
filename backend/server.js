3b9ab4aab60dcc43c50b644c140e13858eba4e1f
wceacses <clubacses@gmail.com>
2020-12-06 22:24:59 +0700

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 require('./models/db');
 const express = require('express');
+const cors = require('cors');
 const employeeController = require('./controllers/employeeController');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.status(200).send('Welcome')
